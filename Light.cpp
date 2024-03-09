9f312d8a5d64ff85bf643a13b14e2cfca0443604
Joe P Shoulak <joe@squarecompass.io>
2024-03-08 18:26:21 -0600

    comments

--- Light.cpp
+++ Light.cpp
@@ -1,27 +1,27 @@
 #include "Arduino.h"
 #include "Light.h"
 
-Light::Light(int pin)
+Light::Light(int pin) // Constructor, requires the pin that controls the light
 {
   _pin = pin;
 }
 
-void Light::begin()
+void Light::begin() // Sets the light's pinMode to output
 {
   pinMode(_pin, OUTPUT);
 }
 
-void Light::set(bool state)
+void Light::set(bool state) // Sets the light to be on if true or off if false
 {
   digitalWrite(_pin, state);
 }
 
-void Light::on()
+void Light::on() // Turns the light on
 {
   this->set(HIGH);
 }
 
-void Light::off()
+void Light::off() // Turns the light off
 {
   this->set(LOW);
 }
--- Light.h
+++ Light.h
@@ -1,19 +1,25 @@
 #ifndef Light_h
 #define Light_h
 
+/*
+  A class for controlling an LED via Arduino digital pins,
+  especially when in strings or other high-current applications
+  where relays or transistors are needed
+*/
+
 class Light
 {
 public:
-  Light(int pin = 2);
+  Light(int pin = 2); // Constructor, requires the pin that controls the light
 
-  void begin();
+  void begin(); // Sets the light's pinMode to output
 
-  void set(bool state);
-  void on();
-  void off();
+  void set(bool state); // Sets the light to be on if true or off if false
+  void on();            // Turns the light on
+  void off();           // Turns the light off
 
 private:
-  int _pin;
+  int _pin; // The pin that controls the LED
 };
 
 #endif
